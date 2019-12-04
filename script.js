df0d3d417d784a78b9e7d9118d7cf0ee18f9b8eb
rishi <rishisingh0831@gmail.com>
2019-12-05 00:57:01 +0530

    Morse code converter added

--- script.js
+++ script.js
@@ -185,6 +185,210 @@ function phoneticToText() {
   res.innerHTML = resString;
 }
 
+// english to morse code converter function here
+
+function englishToMorse() {
+  var newline = "<br />";
+  var results = "";
+
+  var text = document.getElementById("english").value.toUpperCase();
+
+  var res = document.getElementById("enflishToMorse");
+
+  for (var i = 0; i < text.length; i++) {
+    switch (text.charAt(i)) {
+      case "A":
+        results = results + "._ ";
+        break;
+      case "B":
+        results = results + "_... ";
+        break;
+      case "C":
+        results = results + "_._. ";
+        break;
+      case "D":
+        results = results + "_.. ";
+        break;
+      case "E":
+        results = results + ". ";
+        break;
+      case "F":
+        results = results + ".._. ";
+        break;
+      case "G":
+        results = results + "__. ";
+        break;
+      case "H":
+        results = results + ".... ";
+        break;
+      case "I":
+        results = results + ".. ";
+        break;
+      case "J":
+        results = results + ".___ ";
+        break;
+      case "K":
+        results = results + "_._ ";
+        break;
+      case "L":
+        results = results + "._.. ";
+        break;
+      case "M":
+        results = results + "__ ";
+        break;
+      case "N":
+        results = results + "_. ";
+        break;
+      case "O":
+        results = results + "___ ";
+        break;
+      case "P":
+        results = results + ".__. ";
+        break;
+      case "Q":
+        results = results + "__._ ";
+        break;
+      case "R":
+        results = results + "._. ";
+        break;
+      case "S":
+        results = results + "... ";
+        break;
+      case "T":
+        results = results + "_ ";
+        break;
+      case "U":
+        results = results + ".._ ";
+        break;
+      case "V":
+        results = results + "..._ ";
+        break;
+      case "W":
+        results = results + ".__ ";
+        break;
+      case "X":
+        results = results + "_.._ ";
+        break;
+      case "Y":
+        results = results + "_.__ ";
+        break;
+      case "Z":
+        results = results + "__.. ";
+        break;
+      case " ":
+        results = results + newline + newline;
+        break;
+      default:
+        results = results + text.charAt(i) + " ";
+    }
+  }
+
+  res.innerHTML = "<h3>" + results + "</h3>";
+}
+
+//  morse code to english converter here
+
+function morseToEnglish() {
+  var newline = "<br />";
+  var results = "";
+
+  var text = document.getElementById("morse").value.toUpperCase();
+
+  var inputArray = text.split(" ");
+
+  var res = document.getElementById("morseToEnglish");
+
+  for (var i = 0; i < inputArray.length; i++) {
+    switch (inputArray[i]) {
+      case "._":
+        results = results + "A ";
+        break;
+      case "_...":
+        results = results + "B ";
+        break;
+      case "_._.":
+        results = results + "C ";
+        break;
+      case "_..":
+        results = results + "D ";
+        break;
+      case ".":
+        results = results + "E ";
+        break;
+      case ".._.":
+        results = results + "F ";
+        break;
+      case "__.":
+        results = results + "G ";
+        break;
+      case "....":
+        results = results + "H ";
+        break;
+      case "..":
+        results = results + "I ";
+        break;
+      case ".___":
+        results = results + "J ";
+        break;
+      case "_._":
+        results = results + "K ";
+        break;
+      case "._..":
+        results = results + "L ";
+        break;
+      case "__":
+        results = results + "M ";
+        break;
+      case "_.":
+        results = results + "N ";
+        break;
+      case "___":
+        results = results + "O ";
+        break;
+      case ".__.":
+        results = results + "P ";
+        break;
+      case "__._":
+        results = results + "Q ";
+        break;
+      case "._.":
+        results = results + "R ";
+        break;
+      case "...":
+        results = results + "S ";
+        break;
+      case "_":
+        results = results + "T ";
+        break;
+      case ".._":
+        results = results + "U ";
+        break;
+      case "..._":
+        results = results + "V ";
+        break;
+      case ".__":
+        results = results + "W ";
+        break;
+      case "_.._":
+        results = results + "X ";
+        break;
+      case "_.__":
+        results = results + "Y ";
+        break;
+      case "__..":
+        results = results + "Z ";
+        break;
+      case " ":
+        results = results + ".";
+        break;
+      default:
+        results = results + text.charAt(i) + " ";
+    }
+  }
+
+  res.innerHTML = results;
+}
+
 // clicking enter button works as cicking the convert button here
 
 // for decimal textbar
@@ -253,3 +457,21 @@ document
       document.getElementById("btn7").click();
     }
   });
+
+// for english to morse
+
+document.getElementById("english").addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("btn8").click();
+  }
+});
+
+// for morse to english
+
+document.getElementById("morse").addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("btn9").click();
+  }
+});
